5592f6f03071b0dbd637415379604fe4c8c2e2b4
Shah Faisal Wani <robloxfaisal@gmail.com>
2023-05-25 14:42:13 +0700

    final

--- BE/db/db.js
+++ BE/db/db.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "warehouse_new",
+  database: "warehouse",
 });
 
 // Connect to the database
